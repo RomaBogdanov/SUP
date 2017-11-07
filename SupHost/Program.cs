25ee7873e14d19e1b84ebddf34730780781c2f00
Roma <superdiadaroma@rambler.ru>
2017-11-07 15:26:25 +0300

    Первый фикс.

--- SupHost/Program.cs
+++ SupHost/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Хост суп предназначен для:
+/// 1. транзита информации между клиентами и БД;
+/// 2. синхронизации данных от клиентов;
+/// 3. Обсчёта данных (при необходимости).
+/// </summary>
 namespace SupHost
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
