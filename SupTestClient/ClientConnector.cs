7fca24e3e1e8bb9230690b05e9727f8f24f0cd6c
Roma <superdiadaroma@rambler.ru>
2017-11-09 00:50:32 +0300

    Первая реализация сквозного запроса от клиента WPF.

---
+++ SupTestClient/ClientConnector.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using SupTestClient.ClientServiceReference;
+
+namespace SupTestClient
+{
+    /// <summary>
+    /// Класс инкапсюлирующий подключение со стороны клиента к серверу.
+    /// </summary>
+    /// <remarks>
+    /// Реализует паттерн одиночка для организации единого подключения
+    /// из всех точек приложения.
+    /// </remarks>
+    class ClientConnector
+    {
+        private static ClientConnector connector;
+        ITableService tableService;
+        CompositeType compositeType;
+
+        #region Public
+
+        public static ClientConnector CurrentConnector
+        {
+            get
+            {
+                if (connector == null)
+                {
+                    connector = new ClientConnector();
+                    return connector;
+                }
+                return connector;
+            }
+        }
+
+        public DataTable GetTable(TableName tableName)
+        {
+            this.compositeType.TableName = tableName;
+            return this.tableService.GetTable(this.compositeType);
+        }
+
+        #endregion
+
+        #region Private
+
+        private ClientConnector()
+        {
+            this.tableService = new TableServiceClient();
+            this.compositeType = new CompositeType();
+        }
+
+        #endregion
+
+    }
+}
---
+++ SupTestClient/ViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Input;
+using SupTestClient.ClientServiceReference;
+
+namespace SupTestClient
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private List<string> queriesList = new List<string>();
+        private string queryName = "";
+        private DataTable table = new DataTable();
+        private ClientConnector connector;
+
+        #region Public
+
+        public List<string> QueriesList
+        {
+            get
+            {
+                
+                this.queriesList.Add("Hi");
+                this.queriesList.Add("Hello");
+                return queriesList;
+            }
+            set
+            {
+                this.queriesList = value;
+            }
+        }
+
+        public string QueryName
+        {
+            get { return this.queryName; }
+            set
+            {
+                if (this.queryName != value)
+                {
+                    this.queryName = value;
+                    OnPropertyChanged("QueryName");
+                }
+            }
+        }
+
+        public DataTable Table
+        {
+            get
+            {
+                return table;
+            }
+            set
+            {
+                if (this.table !=value)
+                {
+                    this.table = value;
+                    OnPropertyChanged("Table");
+                }
+            }
+        }
+
+        public ICommand ReceiveTable
+        { get; set; }
+
+        public ViewModel()
+        {
+            this.connector = ClientConnector.CurrentConnector;
+            ReceiveTable = new RelayCommand(arg => GetTable());
+        }
+
+        #endregion
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        #region Private
+
+        private void GetTable()
+        {
+            this.Table = this.connector.GetTable(TableName.VisOrders);
+        }
+
+        #endregion
+
+    }
+}
