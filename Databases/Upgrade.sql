f7ad9bb643da6330310fff9c2a5030d915f802f7
gberenov <grigory.berenov@gmail.com>
2018-04-19 11:58:44 +0300

    fix script

--- Databases/Upgrade.sql
+++ Databases/Upgrade.sql
@@ -5,7 +5,10 @@ go
 -- Таймаут для пользователей
 ALTER TABLE vis_new_user
 ADD f_timeout int
+go
 
 UPDATE  vis_new_user SET f_timeout = 180
 --UPDATE  vis_new_user SET f_timeout = -1 WHERE f_user_id=22
+go
+
 
