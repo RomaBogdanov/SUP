ffe4a7b9e6c391d6f42fa5ccba141b20e36ce89e
RE <randomellis@rambler.ru>
2018-04-19 23:10:25 +0700

    Добавил 2 окна для мастера отчётов и придумал не сильно мудрёную логику для их переключения.

---
+++ SupRealClient/Models/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupRealClient.Models
+{
+    public class Route 
+    {
+        public string Name { get; set; }
+
+        public List<string> Points { get; set; }
+    }
+}
