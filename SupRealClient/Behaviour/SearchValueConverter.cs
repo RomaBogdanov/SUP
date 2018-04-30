07c82d34233950b216b7de750221b5aab7e6b8c2
gberenov <grigory.berenov@gmail.com>
2018-04-30 18:19:59 +0300

    Возвращение старого поиска

---
+++ SupRealClient/Behaviour/SearchValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SupRealClient.Behaviour
+{
+    public class SearchValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            string cellText = values[0] == null ? string.Empty :
+                values[0].ToString();
+            string searchText = values[1] as string;
+
+            if (!string.IsNullOrEmpty(searchText) &&
+                !string.IsNullOrEmpty(cellText))
+            {
+                return cellText.ToUpper().Contains(searchText.ToUpper());
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
