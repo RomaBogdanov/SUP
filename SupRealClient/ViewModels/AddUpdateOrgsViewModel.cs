90ef8bf000e04a84979731bcddf767d5cb0e9012
rbogdanov <superdiadaroma@rambler.ru>
2018-05-04 15:47:03 +0300

    Добавление функционала форме добавления и редактирования записей организаций.

--- SupRealClient/ViewModels/AddUpdateOrgsViewModel.cs
+++ SupRealClient/ViewModels/AddUpdateOrgsViewModel.cs
@@ -7,6 +7,9 @@ using System.Windows;
 using System.Windows.Input;
 using SupRealClient.EnumerationClasses;
 using SupRealClient.TabsSingleton;
+using SupRealClient.Common.Interfaces;
+using SupRealClient.Views;
+using System;
 
 namespace SupRealClient.ViewModels
 {
@@ -18,6 +21,10 @@ namespace SupRealClient.ViewModels
         private string name = "";
         private string comment = "";
         private string fullName = "";
+        private string region = "";
+        private string country = "";
+        private Organization organization = new Organization();
+        private IWindow view;
         public int FontSize => GlobalSettings.GetSettings();
 
         public string TypeToolType
@@ -110,13 +117,45 @@ namespace SupRealClient.ViewModels
             }
         }
 
+        public string Region
+        {
+            get { return region; }
+            set
+            {
+                if (value != null)
+                {
+                    region = value;
+                    OnPropertyChanged("Region");
+                }
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                if (value != null)
+                {
+                    country = value;
+                    OnPropertyChanged("Country");
+                }
+            }
+        }
+
         public ICommand Ok { get; set; }
 
         public ICommand Cancel { get; set; }
 
+        public ICommand CountryCommand { get; set; }
+        public ICommand RegionCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+
         public AddUpdateOrgsViewModel()
         {
-            
+            CountryCommand = new RelayCommand(arg => CountyList());
+            RegionCommand = new RelayCommand(arg => RegionsList());
+            ClearCommand = new RelayCommand(arg => Clear(arg as string));
         }
 
         public void SetModel(IAddUpdateOrgsModel addItem1Model)
@@ -126,19 +165,87 @@ namespace SupRealClient.ViewModels
             this.Name = model.Data.Name;
             this.Comment = model.Data.Comment;
             this.FullName = model.Data.FullName;
-            
-            this.Ok = new RelayCommand(arg => this.model.Ok(new Organization
-            {
-                Type = Type,
-                Name = Name,
-                Comment = Comment,
-                FullName = FullName
-            }));
+            organization.Type = Type;
+            organization.Name = Name;
+            organization.Comment = Comment;
+            organization.FullName = FullName;
+            this.Ok = new RelayCommand(arg => this.model.Ok(organization));
             this.Cancel = new RelayCommand(arg => this.model.Cancel());
         }
 
         protected virtual void OnPropertyChanged(string propertyName) =>
             this.PropertyChanged?.Invoke(this,
             new PropertyChangedEventArgs(propertyName));
+
+        private void CountyList()
+        {
+            var result = ViewManager.Instance.OpenWindowModal(
+                "Base4NationsWindView", view) as BaseModelResult;
+            if (result == null)
+            {
+                return;
+            }
+            organization.CountryId = result.Id;
+            organization.Country = result.Name;
+            Country = result.Name;
+
+        }
+
+        private void RegionsList()
+        {
+            var result = ViewManager.Instance.OpenWindowModal(
+                "Base4RegionsWindView", view) as BaseModelResult;
+            if (result == null)
+            {
+                return;
+            }
+            organization.RegionId = result.Id;
+            organization.Region = result.Name;
+            Region = result.Name;
+        }
+
+        private void Clear(string field)
+        {
+            switch (field)
+            {
+                case "Nation":
+                    organization.CountryId = -1;
+                    organization.Country = "";
+                    Country = "";
+                    break;
+                case "Region":
+                    organization.RegionId = -1;
+                    organization.Region = "";
+                    Region = "";
+                    break;
+                    /*
+                case "Organization":
+                    CurrentItem.OrganizationId = -1;
+                    CurrentItem.Organization = "";
+                    break;
+                case "DocType":
+                    CurrentItem.DocumentId = -1;
+                    CurrentItem.DocType = "";
+                    break;
+                case "Department":
+                    CurrentItem.DepartmentId = -1;
+                    CurrentItem.Department = "";
+                    break;
+                case "Cabinet":
+                    CurrentItem.CabinetId = -1;
+                    CurrentItem.Cabinet = "";
+                    break;
+                case "Position":
+                    CurrentItem.Position = "";
+                    break;
+                case "Comment":
+                    CurrentItem.Comment = "";
+                    break;*/
+                default:
+                    return;
+            }
+
+            OnPropertyChanged("CurrentItem");
+        }
     }
 }
