7642f6262c02d338ed29161959a00e6adf3fe5a5
rbogdanov <superdiadaroma@rambler.ru>
2018-05-15 13:16:51 +0300

    Небольшие изменения по внешнему виду списков.

--- SupRealClient/Common/Interfaces/IBase1ViewModel.cs
+++ SupRealClient/Common/Interfaces/IBase1ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SupRealClient.Common.Interfaces
 {
@@ -15,5 +16,21 @@ namespace SupRealClient.Common.Interfaces
         object CurrentItem { get; set; }
         object SelectedValue { get; set; }
         DataGridColumn CurrentColumn { get; set; }
+
+
+        ICommand Begin { get; set; }
+        ICommand Prev { get; set; }
+        ICommand Next { get; set; }
+        ICommand End { get; set; }
+        ICommand Add { get; set; }
+        ICommand Update { get; set; }
+        ICommand Search { get; set; }
+        ICommand Farther { get; set; }
+        //ICommand Ok { get; set; }
+        ICommand Close { get; set; }
+        //ICommand Zones { get; set; }
+        //ICommand Watch { get; set; }
+        //ICommand DoubleClick { get; set; }
+        //ICommand RightClick { get; set; }
     }
 }
