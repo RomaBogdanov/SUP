f3bde5263f5a0f7bba87d3c4ad77e7f9bd5979e1
gberenov <grigory.berenov@gmail.com>
2018-05-18 12:52:38 +0300

    Добавлена маска на отображение и редактирование даты

---
+++ SupRealClient/Behaviour/SupDateConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SupRealClient.Behaviour
+{
+    public class SupDateConverter : IValueConverter
+    {
+        private const string FormatString = "dd.MM.yyyy";
+
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (value != null && (DateTime)value != DateTime.MinValue)
+            {
+                return ((DateTime)value).ToString(FormatString, culture);
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return DateTime.ParseExact((string)value, FormatString, culture);
+            }
+            catch
+            {
+                return DateTime.MinValue;
+            }
+        }
+    }
+}
