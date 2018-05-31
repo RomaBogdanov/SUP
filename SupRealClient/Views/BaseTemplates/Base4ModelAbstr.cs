6a63256cfd8c65db971de2d02480dd4781510d94
Ногтев Алексей <nogtev@pag.local>
2018-05-31 19:15:04 +0300

    Revert "Merge branch 'nogtev' of https://github.com/RomaBogdanov/SUP into nogtev"
    This reverts commit f5d49159363a73c2c828f2bd44003c0f456e7fba, reversing
    changes made to 5fe3aca9533e93a59d2160839dee4451521f5745.

--- SupRealClient/Views/BaseTemplates/Base4ModelAbstr.cs
+++ SupRealClient/Views/BaseTemplates/Base4ModelAbstr.cs
@@ -10,9 +10,6 @@ using SupRealClient.Common.Interfaces;
 using SupRealClient.Common;
 using SupRealClient.TabsSingleton;
 using SupRealClient.Models;
-using SupRealClient.Models.AddUpdateModel;
-using SupRealClient.ViewModels.AddUpdateViewModel;
-using SupRealClient.Views.AddUpdateView;
 
 namespace SupRealClient.Views
 {
@@ -513,6 +510,97 @@ namespace SupRealClient.Views
         }
     }
 
+    public class SpacesListModel<T> : Base4ModelAbstr<T>
+        where T : Space, new()
+    {
+        protected override DataTable Table
+        { get { return SpacesWrapper.CurrentTable().Table; } }
+
+        public SpacesListModel()
+        {
+            SpacesWrapper.CurrentTable().OnChanged += Query;
+            Query();
+            Begin();
+        }
+
+        public override void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoQuery()
+        {
+            Set = new ObservableCollection<T>(
+                from spaces in Table.AsEnumerable()
+                where spaces.Field<int>("f_space_id") != 0
+                select new T
+                {
+                    Id = spaces.Field<int>("f_space_id"),
+                    NumReal = spaces.Field<string>("f_num_real"),
+                    NumBuild = spaces.Field<string>("f_num_build"),
+                    Descript = spaces.Field<string>("f_descript"),
+                    Note = spaces.Field<string>("f_note")
+                });
+        }
+
+        protected override BaseModelResult GetResult()
+        {
+            return new BaseModelResult { Id = CurrentItem.Id, Name = CurrentItem.NumReal };
+
+        }
+    }
+
+    public class DoorsListModel<T> : Base4ModelAbstr<T>
+        where T : Door, new()
+    {
+        protected override DataTable Table
+        { get { return DoorsWrapper.CurrentTable().Table; } }
+
+        public DoorsListModel()
+        {
+            DoorsWrapper.CurrentTable().OnChanged += Query;
+            Query();
+            Begin();
+        }
+
+        public override void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DoQuery()
+        {
+            Set = new ObservableCollection<T>(
+                    from doors in Table.AsEnumerable()
+                    where doors.Field<int>("f_door_id") != 0
+                    select new T
+                    {
+                        Id = doors.Field<int>("f_door_id"),
+                        DoorNum = doors.Field<string>("f_door_num"),
+                        Descript = doors.Field<string>("f_descript"),
+                        SpaceIn = doors.Field<int>("f_space_in"),
+                        SpaceOut = doors.Field<int>("f_space_out"),
+                        AccessPointId = doors.Field<int>("f_access_point_id"),
+                        KeyId = doors.Field<int>("f_key_id")
+                    });
+        }
+
+        protected override BaseModelResult GetResult()
+        {
+            return new BaseModelResult { Id = CurrentItem.Id, Name = CurrentItem.DoorNum };
+        }
+    }
+
     public class AreasListModel<T> : Base4ModelAbstr<T>
         where T : Area, new()
     {
@@ -528,16 +616,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddAreaModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateAreaWindView view = new AddUpdateAreaWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -579,16 +658,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddAreaSpaceModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateAreaSpaceWindView view = new AddUpdateAreaSpaceWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -630,16 +700,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddAccessPointModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateAccessPointWindView view = new AddUpdateAccessPointWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -683,16 +744,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddRealKeyModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateKeyWindView view = new AddUpdateKeyWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -777,16 +829,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddAccessLevelModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateAccessLevelWindView view = new AddUpdateAccessLevelWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -830,16 +873,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddCarModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateCarWindView view = new AddUpdateCarWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
@@ -883,16 +917,7 @@ namespace SupRealClient.Views
 
         public override void Add()
         {
-            AddUpdateAbstrModel model = new AddEquipmentModel();
-            AddUpdateBaseViewModel viewModel = new AddUpdateBaseViewModel
-            {
-                Model = model
-            };
-            AddUpdateEquipmentWindView view = new AddUpdateEquipmentWindView();
-            view.DataContext = viewModel;
-            model.OnClose += view.Handling_OnClose;
-            view.ShowDialog();
-            object res = view.WindowResult;
+            throw new NotImplementedException();
         }
 
         public override void Update()
