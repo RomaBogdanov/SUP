ee21c3206e1911f3ac283a164c32e51acbab04e4
Denis <rda899@yandex.ru>
2018-06-02 20:57:51 +0400

    SUP-216 (Форма "Добавление гражданства")
    Изменено:
    - SupRealClient\ViewModels\ AddItem1ViewModel.cs
    - SupRealClient\Views\ AddItem1View.xaml
    - SupRealClient\Views\ AddItem1View.xaml.cs
    
    Исправил:
    
    1. Форма в соответствии с общим стилем принятым для других форм;
    2. При открытии фокус на введите документы;
    3. таб: введите документы - ок - отмена;
    4. enter: нажатие ок;
    5. заменить введите документ на введите страну

--- SupRealClient/Views/AddItem1View.xaml.cs
+++ SupRealClient/Views/AddItem1View.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using SupRealClient.Models;
 using SupRealClient.ViewModels;
 using System.Windows;
+using System.Windows.Input;
 
 namespace SupRealClient.Views
 {
     /// <summary>
     /// Логика взаимодействия для AddItem1View.xaml
     /// </summary>
-    public partial class AddItem1View : Window
+    public partial class AddItem1View
     {
         public AddItem1View(IAddItem1Model model)
         {
@@ -17,6 +18,19 @@ namespace SupRealClient.Views
             InitializeComponent();
 
             AfterInitialize();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbInputCountry.Focus();
+        }
+
+        private void btnOK_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                btnOK.Command.Execute(null);
+            }
         }
     }
 }
