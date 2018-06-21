ae0b57bd81dd383579ea41fdfdbbac7f5a93bb1a
selyakovSlava <selyakov.vyacheslav@gmail.com>
2018-06-21 21:40:02 +0300

    SUP-266
    Реализация паттерна синглтон для формы "Заявки" в стандарте приложения.
    Очищен BidsView.xaml.cs от реализации лишнего кода.

--- SupRealClient/Views/BidsView.xaml.cs
+++ SupRealClient/Views/BidsView.xaml.cs
@@ -19,51 +19,12 @@ namespace SupRealClient.Views
     /// <summary>
     /// Interaction logic for BidsView.xaml
     /// </summary>
-    public partial class BidsView : IWindow
+    public partial class BidsView
     {
         public BidsView()
         {
-            InitializeComponent();
-
-            
-        }
-
-        public bool CanMinimize { get; private set; } = false;
-
-        public bool IsRealClose { get; set; } = true;
-        public object WindowResult { get; set; }
-
-        public string WindowName { get; private set; } = "BidsView";
-
-        public IWindow ParentWindow { get; set; }
-
-        public void CloseWindow(CancelEventArgs e)
-        {
-            if (!IsRealClose)
-            {
-                IsRealClose = true;
-                e.Cancel = true;
-                this.Hide();
-            }
-        }
-
-        public void Unsuscribe()
-        {
-            this.Closing -= this.Window_Closing;
-        }
-
-        private void Window_Closing(object sender, CancelEventArgs e)
-        {
-            ViewManager.Instance.CloseWindow(this, true, e);
-        }
-
-        private void BidsView_OnKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-            {
-                Close();
-            }
-        }
+            InitializeComponent();            
+        }        
 
         private void MetroWindow_Initialized(object sender, EventArgs e)
         {
