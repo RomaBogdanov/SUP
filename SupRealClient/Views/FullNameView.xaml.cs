0de084a70b3ae2f98b0540c1b6809d77d2820208
Denis <rda899@yandex.ru>
2018-07-25 15:35:37 +0400

    SUP-306
    Проработать форму Организации предназначенную для привязки к организации основной организации. Юзкейс 5.5.1
    
    Пока сделал:
    
    2.	 При открытии фокус на первой организации
    3.	Хоткей вверх перелистывает список вверх (доделать ScrollIntoView)
    4.	Хоткей вниз перелистывает список вниз (доделать ScrollIntoView)
    6.	Фокус постоянно на строке поиска
    10.	Список отсортирован по алфавиту

--- SupRealClient/Views/FullNameView.xaml.cs
+++ SupRealClient/Views/FullNameView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using SupRealClient.ViewModels;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 
 namespace SupRealClient.Views
 {
@@ -9,12 +12,53 @@ namespace SupRealClient.Views
     {
         public FullNameView(FullNameViewModel viewModel)
         {
+            // сортируем список
+            viewModel.Orgs = new ObservableCollection<EnumerationClasses.Organization>
+                                (viewModel.Orgs.OrderBy(o => (o as FullNameOrganization)?.OrgFullName));
+            // селектируем первую организацию
+            if (viewModel.Orgs.Count > 0)
+                viewModel.SelectedOrg = 0;
+
             DataContext = viewModel;
             InitializeComponent();
 
             viewModel.OnClose += Handling_OnClose;
 
             AfterInitialize();
+            tbxSearch.Focus();           
+        }
+
+        private void MetroWindow_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.None)
+            {
+                if (e.Key == Key.Tab)
+                {
+                    e.Handled = true;
+                }
+
+                FullNameViewModel vm = DataContext as FullNameViewModel;
+                if (vm == null)
+                    return;
+
+                if (e.Key == Key.Up && vm.SelectedOrg - 1 >= 0)
+                {                    
+                    vm.SelectedOrg--; 
+                }
+                if (e.Key == Key.Down && vm.SelectedOrg + 1 <= vm.Orgs.Count - 1)
+                {                 
+                    vm.SelectedOrg++;
+                }
+            }
+        }
+
+        private void MetroWindow_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            tbxSearch.Focus();  
+        }
+
+        private void MetroWindow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
             tbxSearch.Focus();
         }
     }
