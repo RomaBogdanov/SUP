900d79a9f1e59e33b24662fc3b1576d5b1cc7a33
gberenov <grigory.berenov@gmail.com>
2018-08-03 19:50:59 +0300

    Добавлен AndoverAgent

---
+++ AndoverLib/IAndoverService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace AndoverLib
+{
+    [ServiceContract]
+    public interface IAndoverService
+    {
+        [OperationContract]
+        string Ping();
+
+        [OperationContract]
+        List<Container> GetContainers();
+
+        [OperationContract]
+        List<Device> GetDevices();
+
+        [OperationContract]
+        List<Area> GetAreas();
+
+        [OperationContract]
+        List<Personnel> GetPersons();
+
+        [OperationContract]
+        List<Schedule> GetSchedules();
+
+        [OperationContract]
+        List<AreaLink> GetAreaLinks();
+
+        [OperationContract]
+        List<Door> GetDoors();
+
+        [OperationContract]
+        List<DoorList> GetDoorLists();
+
+        [OperationContract]
+        void ExportPersons(List<Personnel> persons);
+
+        [OperationContract]
+        bool ExportPersonsDmp(List<PersonInfo> persons);
+
+        [OperationContract]
+        bool ExportPersonDmp(PersonInfo person);
+    }
+}
---
+++ AndoverLib/PersonInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AndoverLib
+{
+    [DataContract]
+    public class PersonInfo
+    {
+        [DataMember]
+        public string UiName { get; set; }
+
+        [DataMember]
+        public string Path { get; set; }
+
+        [DataMember]
+        public string Alias { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string CardNum { get; set; }
+
+        [DataMember]
+        public List<string> Containers { get; set; }
+
+        [DataMember]
+        public List<string> Areas { get; set; }
+
+        [DataMember]
+        public List<string> Schedules { get; set; }
+
+        [DataMember]
+        public bool CreateFolder { get; set; }
+    }
+}
