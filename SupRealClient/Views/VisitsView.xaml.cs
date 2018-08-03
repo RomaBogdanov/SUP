374e9131946b241cac4e94d5c04dff0441a8dc91
gberenov <grigory.berenov@gmail.com>
2018-08-03 17:05:40 +0300

    Наработки по открытию заявки по двойному щелчку

--- SupRealClient/Views/VisitsView.xaml.cs
+++ SupRealClient/Views/VisitsView.xaml.cs
@@ -64,6 +64,7 @@ namespace SupRealClient.Views
         private int selectedMainDocument = -1;
         private int selectedDocument = -1;
         private int selectedCard = -1;
+        private int selectedOrder = -1;
         private const string _nameDocument_PhotoImageType = "Личная фотография";
 	    private const string _nameDocument_SignatureImageType = "Личная подпись";
 	    private bool _visibleButton_OpenDocument_InRedactMode = false;
@@ -255,6 +256,16 @@ namespace SupRealClient.Views
             }
         }
 
+        public int SelectedOrder
+        {
+            get { return selectedOrder; }
+            set
+            {
+                selectedOrder = value;
+                OnPropertyChanged();
+            }
+        }
+
         public string PhotoSource
         {
             get { return Model?.PhotoSource; }
@@ -282,6 +293,7 @@ namespace SupRealClient.Views
 
         public ICommand ExtraditeCommand { get; set; }
         public ICommand ReturnCommand { get; set; }
+        public ICommand OpenOrderCommand { get; set; }
 
         public ICommand OkCommand { get; set; }
         public ICommand CancelCommand { get; set; }
@@ -350,7 +362,7 @@ namespace SupRealClient.Views
 
 		    ExtraditeCommand = new RelayCommand(obj => Extradite());
 		    ReturnCommand = new RelayCommand(obj => Return());
-
+            OpenOrderCommand = new RelayCommand(obj => OpenOrder());
 
             AddImageSourceCommand = new RelayCommand(arg => AddImageSource(ImageType.Photo, _nameDocument_PhotoImageType));
             RemoveImageSourceCommand= new RelayCommand(arg => RemoveImageSource(ImageType.Photo, _nameDocument_PhotoImageType));
@@ -643,6 +655,16 @@ namespace SupRealClient.Views
             };
 
             window.ShowDialog();
+        }
+
+        private void OpenOrder()
+        {
+            if (SelectedOrder < 0)
+            {
+                return;
+            }
+
+            ViewManager.Instance.OpenWindow("BidsView");
         }
 
         private void Edit()
