1cf5a45b59dff74efede6b8a91620d6e1968a2e6
IMATIC\lipatov.v <lipatov.v@imatic.pro>
2018-08-03 10:14:17 +0300

    Реализация передвижения фокуса по нажатию Enter (реализация двойным нажатием)

--- SupRealClient/Views/AddUpdateBidWindView.xaml.cs
+++ SupRealClient/Views/AddUpdateBidWindView.xaml.cs
@@ -28,12 +28,27 @@ namespace SupRealClient.Views
         /// </summary>
         private readonly TraversalRequest _focusMover = new TraversalRequest(FocusNavigationDirection.Next);
 
+	    private List<UIElement> enterUiElementsSequence;
+	    private UIElement previousEnterUiElement = null;
+
         public object WindowResult { get; set; }
 
         public AddUpdateBidWindView()
         {
             InitializeComponent();
-        }        
+	        enterUiElementsSequence = new List<UIElement>
+	        {
+		        btnSelectBid,
+		        btnSelectOrganization,
+		        tbPosition,
+		        btnSelectCatcher,
+		        dpTimeFrom,
+		        dpTimeTo,
+		        btnSelectPass,
+		        checkDisable,
+		        btnOK
+	        };
+		}        
 
         public void Handling_OnClose(object result)
         {
@@ -46,12 +61,53 @@ namespace SupRealClient.Views
             btnSelectBid.Focus();
         }
 
+	    private void uiElement_PreviewKeyDown(object sender, KeyEventArgs e)
+	    {
+		    if (e.Key != Key.Enter)
+		    {
+				return;
+		    }
+
+		    if (previousEnterUiElement !=null && previousEnterUiElement.Equals(sender))
+		    {
+			    UIElement newFocus = previousEnterUiElement;
+			    while (true)
+			    {
+				    int index = enterUiElementsSequence.FindIndex(x => x.Equals(newFocus));
+				    index = (index + 1) % enterUiElementsSequence.Count;
+					newFocus = enterUiElementsSequence[index];
+				    newFocus?.Focus();
+
+				    if (newFocus != null && !newFocus.IsEnabled)
+				    {
+						continue;
+				    }
+
+				    break;
+			    }
+			   
+			    e.Handled = true;
+		    }
+		    else
+		    {
+			    previousEnterUiElement = (UIElement)sender;
+			}
+	    }
+
         private void btnSelectBid_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-	            btnSelectOrganization.Focus();
-                e.Handled = true;
+	            if (!Equals(previousEnterUiElement, btnSelectBid))
+	            {
+		            previousEnterUiElement = btnSelectBid;
+	            }
+	            else
+	            {
+		            previousEnterUiElement = null;
+					btnSelectOrganization.Focus();
+					e.Handled = true;
+				}
             }
         }
 
@@ -59,18 +115,35 @@ namespace SupRealClient.Views
 	    {
 		    if (e.Key == Key.Enter)
 		    {
-			    btnSelectCatcher.Focus();
-			    e.Handled = true;
-		    }
+				if (!Equals(previousEnterUiElement, btnSelectOrganization))
+				{
+					previousEnterUiElement = btnSelectOrganization;
+				}
+				else
+				{
+					previousEnterUiElement = null;
+					btnSelectCatcher.Focus();
+					e.Handled = true;
+				}
+			}
 		}
 
         private void btnSelectCatcher_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+	        bool test = Equals((UIElement) sender, btnSelectCatcher); 
             if (e.Key == Key.Enter)
             {
-                dpTimeFrom.Focus();
-                e.Handled = true;
-            }
+				if (!Equals(previousEnterUiElement, btnSelectCatcher))
+				{
+					previousEnterUiElement = btnSelectCatcher;
+				}
+				else
+				{
+					previousEnterUiElement = null;
+					dpTimeFrom.Focus();
+					e.Handled = true;
+				}
+			}
         }
 
         private void UIdef_KeyDown(object sender, KeyEventArgs e)
