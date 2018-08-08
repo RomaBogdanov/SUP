6b740f80a9d7814329568dafe7f6ff090095fa58
IMATIC\tarasov.r <tarasov.r@imatic.pro>
2018-08-08 20:06:17 +0300

    SUP-391, SUP-392 - нельзя добавлять/изменять имя документа из пустоты и только пробелов

--- SupRealClient/Models/AddItemDocumentsModel.cs
+++ SupRealClient/Models/AddItemDocumentsModel.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using System.Data;
+using System.Windows;
 using SupClientConnectionLib;
 using SupRealClient.Common.Data;
 using SupRealClient.TabsSingleton;
 
 namespace SupRealClient.Models
 {
-    /// <summary>
-    /// Добавление документа - модель
-    /// </summary>
-    public class AddItemDocumentsModel : IAddItem1Model
-    {
-        public event Action OnClose;
+	/// <summary>
+	/// Добавление документа - модель
+	/// </summary>
+	public class AddItemDocumentsModel : IAddItem1Model
+	{
+		public event Action OnClose;
 
-        public FieldData Data { get { return new FieldData(); } }
+		public FieldData Data
+		{
+			get { return new FieldData(); }
+		}
 
-        public void Ok(FieldData data)
-        {
-            DocumentsWrapper documents = DocumentsWrapper.CurrentTable();
-            if (data.Field != "")
-            {
-                DataRow row = documents.Table.NewRow();
-                row["f_doc_name"] = data.Field;
-                row["f_deleted"] = "N";
-                row["f_rec_date"] = DateTime.Now;
-                row["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                documents.Table.Rows.Add(row);
-            }
-            Cancel();
-        }
+		public void Ok(FieldData data)
+		{
+			DocumentsWrapper documents = DocumentsWrapper.CurrentTable();
+			if (!string.IsNullOrWhiteSpace(data.Field))
+			{
+				DataRow row = documents.Table.NewRow();
+				row["f_doc_name"] = data.Field;
+				row["f_deleted"] = "N";
+				row["f_rec_date"] = DateTime.Now;
+				row["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
+				documents.Table.Rows.Add(row);
 
-        public void Cancel()
-        {
-            OnClose?.Invoke();
-        }
-    }
+				Cancel();
+			}
+			else
+			{
+				MessageBox.Show($"Введите корректное имя документа\nИмя документа не может быть пустым, не должно содержать только пробелы", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+			}
+		}
+
+		public void Cancel()
+		{
+			OnClose?.Invoke();
+		}
+	}
 }
--- SupRealClient/Models/UpdateItemDocumentsModel.cs
+++ SupRealClient/Models/UpdateItemDocumentsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Windows;
 using SupClientConnectionLib;
 using SupRealClient.EnumerationClasses;
 using SupRealClient.Common.Data;
@@ -7,40 +8,49 @@ using SupRealClient.TabsSingleton;
 
 namespace SupRealClient.Models
 {
-    /// <summary>
-    /// Обновление документа - модель
-    /// </summary>
-    class UpdateItemDocumentsModel : IAddItem1Model
-    {
-        private Document document;
+	/// <summary>
+	/// Обновление документа - модель
+	/// </summary>
+	class UpdateItemDocumentsModel : IAddItem1Model
+	{
+		private Document document;
 
-        public FieldData Data { get { return new FieldData { Field = document.DocName }; } }
+		public FieldData Data
+		{
+			get { return new FieldData {Field = document.DocName}; }
+		}
 
-        public event Action OnClose;
+		public event Action OnClose;
 
-        public UpdateItemDocumentsModel(Document document)
-        {
-            this.document = document;
-        }
+		public UpdateItemDocumentsModel(Document document)
+		{
+			this.document = document;
+		}
 
-        public void Ok(FieldData data)
-        {
-            DocumentsWrapper documents = DocumentsWrapper.CurrentTable();
-            if (data.Field != "")
-            {
-                DataRow dataRow = documents.Table.Rows.Find(document.Id);
-                dataRow.BeginEdit();
-                dataRow["f_doc_name"] = data.Field;
-                dataRow["f_rec_date"] = DateTime.Now;
-                dataRow["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                dataRow.EndEdit();
-            }
-            Cancel();
-        }
+		public void Ok(FieldData data)
+		{
 
-        public void Cancel()
-        {
-            OnClose?.Invoke();
-        }
-    }
+			DocumentsWrapper documents = DocumentsWrapper.CurrentTable();
+			if (!string.IsNullOrWhiteSpace(data.Field))
+			{
+				DataRow row = documents.Table.Rows.Find(document.Id);
+				row["f_doc_name"] = data.Field;
+				row["f_deleted"] = "N";
+				row["f_rec_date"] = DateTime.Now;
+				row["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
+				row.EndEdit();
+
+				Cancel();
+			}
+			else
+			{
+				MessageBox.Show($"Введите корректное имя документа\nИмя документа не может быть пустым, не должно содержать только пробелы", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+			}
+		}
+
+		public void Cancel()
+		{
+			OnClose?.Invoke();
+		}
+	}
 }
