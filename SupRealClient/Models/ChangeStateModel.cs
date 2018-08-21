30d1462fecd8dab4a0fbd9bcce2b3e51af42b86e
gberenov <grigory.berenov@gmail.com>
2018-08-21 12:17:32 +0300

    Изменение состояния пропуска: логика вынесена в отдельный класс

--- SupRealClient/Models/ChangeStateModel.cs
+++ SupRealClient/Models/ChangeStateModel.cs
@@ -1,5 +1,6 @@
 ﻿using SupClientConnectionLib;
 using SupRealClient.EnumerationClasses;
+using SupRealClient.Models.Helpers;
 using SupRealClient.TabsSingleton;
 using System;
 using System.Data;
@@ -80,83 +81,7 @@ namespace SupRealClient.Models
 
         public void Ok(Card data)
         {
-            CardsExtWrapper cards = CardsExtWrapper.CurrentTable();
-            DataRow row = null;
-            foreach (DataRow r in cards.Table.Rows)
-            {
-                if (r.Field<int>("f_object_id_hi") == data.CardIdHi &&
-                    r.Field<int>("f_object_id_lo") == data.CardIdLo)
-                {
-                    row = r;
-                }
-            }
-            int prevStateId = -1;
-            if (row == null)
-            {
-                row = cards.Table.NewRow();
-                row["f_object_id_hi"] = data.CardIdHi;
-                row["f_object_id_lo"] = data.CardIdLo;
-                row["f_state_id"] = data.StateId;
-                row["f_create_date"] = data.CreateDate;
-                row["f_comment"] = "";
-                row["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                row["f_rec_date"] = DateTime.Now;
-                row["f_lost_date"] = DateTime.MinValue;
-                row["f_last_visit_id"] = 0;
-                row["f_deleted"] = "N";
-                cards.Table.Rows.Add(row);
-            }
-            else
-            {
-                prevStateId = row.Field<int>("f_state_id");
-                row.BeginEdit();
-                row["f_state_id"] = data.StateId;
-                row["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                row["f_rec_date"] = DateTime.Now;
-                row["f_deleted"] = "N";
-                row.EndEdit();
-            }
-
-            if (data.StateId == (int)CardState.Active && prevStateId == (int)CardState.Issued)
-            {
-                ReturnCard(data);
-            }
-
-            foreach (DataRow r in CardAreaWrapper.CurrentTable().Table.Rows)
-            {
-                if (r.Field<string>("f_deleted") == "N" &&
-                    r.Field<int>("f_card_id_hi") == data.CardIdHi &&
-                    r.Field<int>("f_card_id_lo") == data.CardIdLo)
-                {
-                    r.BeginEdit();
-                    r["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                    r["f_rec_date"] = DateTime.Now;
-                    r["f_deleted"] = "Y";
-                    r.EndEdit();
-                }
-            }
-
-            // TODO - здесь в Andover выгружается пропуск с пустым списком областей доступа
-            // Data.CurdNum
-
-            Cancel(data.StateId);
-        }
-
-        private void ReturnCard(Card data)
-        {
-            foreach (DataRow r in VisitsWrapper.CurrentTable().Table.Rows)
-            {
-                if (r.Field<string>("f_deleted") == "N" &&
-                    r.Field<int>("f_card_id_hi") == data.CardIdHi &&
-                    r.Field<int>("f_card_id_lo") == data.CardIdLo)
-                {
-                    r.BeginEdit();
-                    r["f_rec_operator"] = Authorizer.AppAuthorizer.Id;
-                    r["f_rec_date"] = DateTime.Now;
-                    r["f_deleted"] = "Y";
-                    r.EndEdit();
-                }
-            }
+            Cancel(ChangeStateHelper.ChangeState(data));
         }
     }
 }
