2d34d532e00583c3a5819856d584f3a9f8e5df47
gberenov <grigory.berenov@gmail.com>
2018-08-24 19:13:33 +0300

    Выдача пропусков, если уже есть выданные/утерянные

---
+++ SupRealClient/Views/Visitor/DeactivateCardsView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SupRealClient.EnumerationClasses;
+using SupRealClient.TabsSingleton;
+using SupRealClient.ViewModels;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows.Input;
+
+namespace SupRealClient.Views.Visitor
+{
+    /// <summary>
+    /// Interaction logic for DeactivateCardsView.xaml
+    /// </summary>
+    public partial class DeactivateCardsView
+    {
+        public DeactivateCardsView(DeactivateCardsViewModel viewModel)
+        {
+            viewModel.OnClose += () => this.Close();
+            this.DataContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ SupRealClient/Views/Visitor/IssuedCardsMessageBoxView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using SupRealClient.ViewModels;
+
+namespace SupRealClient.Views.Visitor
+{
+    /// <summary>
+    /// Interaction logic for IssuedCardsMessageBoxView.xaml
+    /// </summary>
+    public partial class IssuedCardsMessageBoxView
+    {
+        public IssuedCardsMessageBoxView(IssuedCardsMessageBoxViewModel viewModel)
+        {
+            viewModel.OnClose += () => this.Close();
+            this.DataContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
